8d136c1044e6e54516d8fb9afab14bd626fdebfa
Louis Hampton <louis@switch-ev.com>
2022-11-30 17:46:49 +0000

    Add --dry-run support

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,12 @@ use serde::{Deserialize, Serialize};
 #[clap(name = "cargo")]
 #[clap(bin_name = "cargo")]
 pub enum Command {
-    #[clap(name = "ktra-login")]
-    #[clap(about, author, version)]
     KtraLogin(KtraLoginOpt),
 }
 
 /// Automated login for private ktra registries
 #[derive(Debug, Clone, clap::Args)]
+#[clap(author, about, long_about = None, version)]
 pub struct KtraLoginOpt {
     #[clap(flatten)]
     manifest: Manifest,
@@ -28,6 +27,9 @@ pub struct KtraLoginOpt {
     username: String,
     /// The account password to log in with
     password: String,
+    /// Check that the manifest is valid and that the remote registry exists, but don't generate a token
+    #[clap(long)]
+    dry_run: bool,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -57,9 +59,9 @@ fn main() -> Result<(), anyhow::Error> {
     let config = Config::default()?;
     let ws = Workspace::new(&manifest, &config)?;
     let config = ws.config();
-    let registry = config
-        .default_registry()?
-        .ok_or(anyhow::anyhow!("No default registry is set"))?;
+    let registry = config.default_registry()?.ok_or(anyhow::anyhow!(
+        "No default registry is set for this package"
+    ))?;
     let source_id = SourceId::alt_registry(config, &registry)?;
     let lock = config.acquire_package_cache_lock()?;
     let mut registry_source = RegistrySource::remote(source_id, &HashSet::new(), config)?;
@@ -73,26 +75,35 @@ fn main() -> Result<(), anyhow::Error> {
     drop(lock);
 
     let request = Client::new();
+    // In a dry run, don't include the password field to avoid token generation
     let remote_resp: KtraResponse = request
         .post(format!("{}/ktra/api/v1/login/{}", api, login.username))
         .header("content-type", "application/json")
-        .body(format!("{{\"password\": \"{}\"}}", login.password))
+        .body(if !login.dry_run {
+            format!("{{\"password\": \"{}\"}}", login.password)
+        } else {
+            "{}".to_owned()
+        })
         .send()?
         .json()?;
 
-    match remote_resp {
-        KtraResponse::Errors(e) => {
-            let error = e
-                .into_iter()
-                .map(|e| e.detail)
-                .collect::<Vec<String>>()
-                .join(", ");
-            Err(anyhow::anyhow!(error))?
+    if login.dry_run {
+        Ok(())
+    } else {
+        match remote_resp {
+            KtraResponse::Errors(e) => {
+                let error = e
+                    .into_iter()
+                    .map(|e| e.detail)
+                    .collect::<Vec<String>>()
+                    .join(", ");
+                Err(anyhow::anyhow!(error))?
+            }
+            KtraResponse::Token(token) => Ok(cargo::ops::registry_login(
+                config,
+                Some(token),
+                Some(registry),
+            )?),
         }
-        KtraResponse::Token(token) => Ok(cargo::ops::registry_login(
-            config,
-            Some(token),
-            Some(registry),
-        )?),
     }
 }
